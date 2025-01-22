c822245e8e635f2eb34f29216e3a1703948cf63b
deivison556 <deivison10gracindo@outlook.com>
2025-01-22 14:54:35 -0300

    Aula 02 - O que são variaveis - concluido

---
+++ 01 - Modulo de JavaScript/01 - Aulas do Modulo I/02 - O que são variaveis/app.js
@@ -0,0 +1,25 @@
+// O Que são variáveis
+/*
+  Variáveis servem para armazenar os dados 
+  para que possamos usá-los em nosso código
+*/
+
+// 01 - Criando variáveis
+// Existem 3 formas de criar variáveis: var, let e const
+var nome = 'Deivison'; // var é o menos utilizado hoje em dia
+console.log(nome);
+
+let sobrenome = 'Gracindo'; // let é o mais utilizado hoje em dia
+console.log(sobrenome);
+
+const idade = 27; // const é uma variavel constante que o dado não muda
+console.log(idade);
+
+/*
+  02 - Nomeando variáveis corretamente
+
+  - Nomes que tenham significados
+  - Utilizando a técnica camelcase
+
+  exemplo de camelcase = let nomeDeUsuario;
+*/
