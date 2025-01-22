1913f8405343a93b9e840373b462ccefa40c2a88
deivison556 <deivison10gracindo@outlook.com>
2025-01-22 14:50:49 -0300

    Aula 01 - Tipos de dados em JavaScript - concluido

---
+++ 01 - Modulo de JavaScript/01 - Aulas do Modulo I/01 - Tipos de dados em JavaScript/app.js
@@ -0,0 +1,26 @@
+// Tipos de dados primitivos
+// tipo number
+let numero = 1;
+let numero2 = 5;
+let number3 = 35.5;
+let number4 = -150.1;
+
+// Utilizando o bigint que são para numeros gigantes
+let numeroGrande = 5555555555555555555555555555n; // coloca o n no final 
+
+// tipo string
+let nome = 'Deivison';
+let numeroEmString = '23';
+
+// tipo boolean - true & false
+let booleanFalse = false;
+let booleanTrue = true;
+
+// tipo null e undefined
+let nulo = null; // retorna null
+let naoDefinido; // retorna undefirned por esta sem valor
+console.log(typeof nulo);
+console.log(typeof naoDefinido);
+
+// Tipos de dados não primitivo
+// objetos e funções
