d3194423d3a5e9d3fdd7149b8574ddf091fba95f
deivison556 <deivison10gracindo@outlook.com>
2025-01-29 17:59:30 -0300

    Aula 04 - Comentarios em JavaScript - concluido

---
+++ 01 - Modulo de JavaScript/01 - Aulas do Modulo I/04 - Comentários em JavaScript/app.js
@@ -0,0 +1,20 @@
+/* COMENTÁRIOS EM JAVASCRIPT  
+  // Os comentários são linhas do código que serão ignoradas pelo interpretador.
+
+  --- COMO USAR OS COMENTÁRIOS ---
+
+    - Comentário com uma linha utilizar o //
+    - Comentário com vários linhas utilizar dessa forma  
+
+*/
+
+let nome = 'Deivison'; //comentário com uma linha
+let sobrenome = 'neri'; /* comentario com varias linhas */
+
+/* PARA QUE SERVEM OS COMENTÁRIOS 
+
+  - Deixar anotações
+  - Ajuda a entender o código
+  - Documentar o código
+
+*/
