f8c5bf4d5b442a0b1b8f4b3e580b8155f38252f4
deivison556 <deivison10gracindo@outlook.com>
2025-05-07 17:18:28 -0300

    Aula 06 - Recursos nativos do navegador - concluído

---
+++ 01 - Modulo de JavaScript/01 - Aulas do Modulo I/06 - Recursos nativos do navegador/app.js
@@ -0,0 +1,9 @@
+let nome = 'Deivison';
+
+console.log(nome);
+console.error('Aqui tem um erro');
+console.warn('ALERTA');
+
+// window.alert('Seja bem-vindo');
+let idade = Number(window.prompt('Digite a sua idade:'));
+let confirma = window.confirm('Deseja entra no site?');
