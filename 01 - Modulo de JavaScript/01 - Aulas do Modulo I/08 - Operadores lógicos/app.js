88554b5db489cce32127516de855e3474f4e2358
deivison556 <deivison10gracindo@outlook.com>
2025-05-09 16:27:14 -0300

    08 - Operadores lógicos - concluído

---
+++ 01 - Modulo de JavaScript/01 - Aulas do Modulo I/08 - Operadores lógicos/app.js
@@ -0,0 +1,54 @@
+/* OPERADORES LÓGICOS
+
+  - AND &&
+  - OR ||
+  - NOT !
+
+*/
+
+/* 1° OPERADOR AND 
+
+  true && true = true
+  true && false = false
+  false && true = false
+  false && false = false
+
+  Ambos precisam ser true para que a comparação se torne verdadeira
+
+*/
+
+// Utilizando operador and
+let n1 = true && true; // retorna true
+let n2 = true && false; // retorna false
+let n3 = false && true; // retorna false
+let n4 = false && false; // retorna false
+
+/* 2° OPERADOR OR 
+
+  true || true = true
+  true || false = true
+  false || true = true
+  false || false = false
+
+  Basta apenas um ser verdadeiro para que seja true.
+
+*/
+
+// Utilizando operador and
+let n5 = true || true; // retorna true
+let n6 = true || false; // retorna true
+let n7 = false || true; // retorna true
+let n8 = false || false; // retorna false
+
+/* 3° OPERADOR NOT 
+
+  Inverte o resultado (de verdadeiro para falso e vice-versa)
+
+*/
+
+// Utilizando operador and
+let n9 = !9 < 5; // retorna true inverteu o valor
+let n10 = !12 >= 12; // retorna false inverteu o valor
+
+
+
